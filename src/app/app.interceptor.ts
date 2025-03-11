dc94167f614c26211c15dffa52f5979183617da0
fitahiana-mahery <mahery_fitahiana@yahoo.com>
2025-03-11 21:33:27 +0300

    [FEATURE] cookie done

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -5,7 +5,7 @@ import {
 export const appInterceptor: HttpInterceptorFn = (req, next) => {
   // Clone the request to add the authentication header.
   const newReq = req.clone({
-
+      withCredentials: true, // Ensures cookies are sent with the request
   });
   return next(newReq);
 };
