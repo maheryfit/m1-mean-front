1b27107dd84a677ffc4b7870e3baa03cf4916a1f
Eriq606 <erohweltall@gmail.com>
2025-03-28 11:09:15 +0300

    [STYLE] formulaire d'inscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,26 @@ import {logout} from './features/connect.guard';
 import {AppComponent} from './app.component';
 import { DashboardClientComponent } from './dashboard-client/dashboard-client.component';
 import { LoginComponent } from './core/auth/pages/login/login.component';
+import { SignupComponent } from './core/auth/pages/signup/signup.component';
 
 export const routes: Routes = [
   {
     path: 'login',
+    title: 'Connexion',
     // loadChildren: () => import('./core/auth/auth.routes').then(r => r.authRoutes)
     component: LoginComponent
+  },
+  {
+    path: "",
+    // redirectTo:"login",
+    // pathMatch: "full",,
+    title: "Connexion",
+    component: LoginComponent
+  },
+  {
+    path: "sign-up",
+    title: "Inscription",
+    component: SignupComponent
   },
   {
     path: 'chatting',
@@ -18,12 +32,6 @@ export const routes: Routes = [
     path: 'logout',
     component: AppComponent,
     canActivate: [logout]
-  },
-  {
-    path: "",
-    // redirectTo:"login",
-    // pathMatch: "full",
-    component: LoginComponent
   },
   {
     path: "client",
