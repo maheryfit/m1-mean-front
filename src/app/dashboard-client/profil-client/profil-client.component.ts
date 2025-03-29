1f48099686af6514e6e3475d9e8f8ad4a6c796b6
Eriq606 <erohweltall@gmail.com>
2025-03-29 16:56:22 +0300

    [STYLE] choisir abonnement profil

--- src/app/dashboard-client/profil-client/profil-client.component.ts
+++ src/app/dashboard-client/profil-client/profil-client.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { AbonnementComponent } from './abonnement/abonnement.component';
 
 @Component({
   selector: 'app-profil-client',
-  imports: [],
+  imports: [AbonnementComponent],
   templateUrl: './profil-client.component.html',
   styleUrl: './profil-client.component.css'
 })
 export class ProfilClientComponent {
-
+  abonnements=[
+    {
+      id:"1",
+      nom:"bronze",
+      couleur:"btn-outline-secondary",
+      prix:"5000",
+      checked:false
+    },
+    {
+      id:"2",
+      nom:"silver",
+      couleur:"btn-outline-light",
+      prix:"10000",
+      checked:true
+    },
+    {
+      id:"3",
+      nom:"gold",
+      couleur:"btn-outline-warning",
+      prix:"15000",
+      checked:false
+    }
+  ]
 }
