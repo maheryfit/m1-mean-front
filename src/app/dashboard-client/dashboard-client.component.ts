34f5ad33a80906a497a2045ecc8942d89d588374
Eriq606 <erohweltall@gmail.com>
2025-04-02 10:16:44 +0300

    [DEV] déconnexion

--- src/app/dashboard-client/dashboard-client.component.ts
+++ src/app/dashboard-client/dashboard-client.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { environment } from '../../environments/environment.development';
+import { AuthService } from '../core/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard-client',
@@ -9,5 +10,12 @@ import { environment } from '../../environments/environment.development';
   styleUrl: './dashboard-client.component.css'
 })
 export class DashboardClientComponent {
+  loginService=inject(AuthService);
+  router=inject(Router);
   brand=environment.BRAND;
+  nomUtilisateur=localStorage.getItem("nom_utilisateur");
+  logout(){
+    this.loginService.logout().subscribe();
+    this.router.navigate(["login"]);
+  }
 }
