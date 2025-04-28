6aba1a4fc931146c3dd1af6b1b2e46b5fa051a56
Eriq606 <erohweltall@gmail.com>
2025-04-28 03:14:43 +0300

    [DEPLOY] déploiement nginx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   HOST_URL: "http://localhost:4200",
-  API_URL: "http://localhost:5000/",
+  API_URL: "http://46.101.29.136:3000",
   WEBSOCKET_URL: "ws://localhost:3000",
   FOOTER: "HERILALAO Ny Avo Lucas - Mahery",
   BRAND: "HandyMan",
