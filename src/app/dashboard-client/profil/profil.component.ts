ad255813026a122cb57900bacb373d5c7e63267f
Eriq606 <erohweltall@gmail.com>
2025-05-04 10:04:36 +0300

    [DEV] liste abonnements page profil

--- src/app/dashboard-client/profil/profil.component.ts
+++ src/app/dashboard-client/profil/profil.component.ts
@@ -1,6 +1,8 @@
 import {Component, inject} from '@angular/core';
 import {ClientService} from '../../services/client.service';
 import {Client} from '../../models/client.model';
+import {Abonnement} from '../../models/abonnement.model';
+import { AbonnementService } from '../../services/abonnement.service';
 
 @Component({
   selector: 'app-profil',
@@ -10,10 +12,15 @@ import {Client} from '../../models/client.model';
 })
 export class ProfilComponent {
   clientService=inject(ClientService);
+  abonnementService=inject(AbonnementService);
   client:Client|null = null;
+  abonnements:Abonnement[]=[];
   constructor(){
     this.clientService.detailsProfil()
       .then(response => this.client = response)
+      .catch(error => alert(error));
+    this.abonnementService.getAbonnements()
+      .then(response => this.abonnements = response)
       .catch(error => alert(error));
   }
 }
